5963ee2f20fe4c02c698545a3b78f471b95f096c
sdygt <root@sdygt.net>
2018-10-08 15:47:45 +0800

    Fix run-moloch.sh && Add docker-compose.yml

--- scripts/run-moloch.sh
+++ scripts/run-moloch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f "/data/.ES_INITED"];then
+if [ ! -f "/data/.ES_INITED" ];then
     sed -i -e "s,MOLOCH_ELASTICSEARCH,${MOLOCH_ELASTICSEARCH},g" /data/moloch/etc/config.ini
     /data/moloch/db/db.pl ${MOLOCH_ELASTICSEARCH} init
     touch /data/.ES_INITED
