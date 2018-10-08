2b0c8f73683178ec6fdae819eca63728cedddc4d
sdygt <root@sdygt.net>
2018-10-08 16:32:01 +0800

    Wait for ES to start

--- scripts/run-moloch.sh
+++ scripts/run-moloch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-if [ ! -f "/data/.ES_INITED" ];then
+echo "Running run-moloch.sh"
+echo ${MOLOCH_ELASTICSEARCH}
+/data/wait-for-it.sh ${MOLOCH_ELASTICSEARCH}
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+if [ ! -f "/data/.ES_INITED" ]; then
     sed -i -e "s,MOLOCH_ELASTICSEARCH,${MOLOCH_ELASTICSEARCH},g" /data/moloch/etc/config.ini
     /data/moloch/db/db.pl ${MOLOCH_ELASTICSEARCH} init
     touch /data/.ES_INITED
